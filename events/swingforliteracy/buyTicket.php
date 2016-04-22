c0548d1ec04f8814609eacaed07693ecf9e9c8bc
Adam Coard <acoard@advantcomp.com>
2016-04-21 20:40:15 -0700

    PHP pricing logic for stripe transactions.

--- events/swingforliteracy/buyTicket.php
+++ events/swingforliteracy/buyTicket.php
@@ -4,23 +4,40 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/payment/php/stripe_simple.php');
 
 $token  = $_POST['stripeToken'];
 $email = $_POST['email'];
+$identifier = $_POST['identifier'];
 $description = $_POST['description'];
 
 
-if (!(isset($token) && isset($email))){
+if (!(isset($token) && isset($email) && isset($identifier) )){
 	echo "Get out of here man.";
+	die;
 }
 else {
-	// echo "you're in";
-	// die;
 	$customer = \Stripe\Customer::create(array(
 	    'email' => $email,
 	    'card'  => $token
 	));
 
+	switch $identifier {
+		case "swing_for_literacy_single":
+			$amount = 2000;
+			break;
+		case "swing_for_literacy_couple":
+			$amount = 3500;
+			break;
+		case "swing_for_literacy_student":
+			$amount = 1500;
+			break;
+		case "swing_for_literacy_rhs":
+			$amount = 1500;
+			break;
+		default:
+			$amount = 2000;
+	}
+
 	$charge = \Stripe\Charge::create(array(
 	    'customer' => $customer->id,
-	    'amount'   => 2000,
+	    'amount'   => $amount,
 	    'currency' => 'cad',
 	    'description' => $description,
 	));
